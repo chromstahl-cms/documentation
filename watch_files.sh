9c87a47007d7ba94b2266fab17661c75f3e0203f
Lars Grahmann <lars@kloud.software>
2019-05-06 16:49:50 +0200

    add warning if inotify is missing

--- watch_files.sh
+++ watch_files.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+if ! [ -x "$(command -v inotifywait)" ]; then
+    echo 'Error: "inotify-tools" is not installed.' >&2
+    echo 'Please install it and try again.' >&2
+    exit 1
+fi
+
 inotifywait -m -r -e close_write --format '%w%f' "." | while read MODIFIED
 do
     if [[ $MODIFIED == *.tex ]] ;
